8951cdb815540b2b01a8f0dd860660c10592140e
KhurasMak <makhurakgotlello@gmail.com>
2023-12-08 17:49:45 +0200

    printf

---
+++ print_%.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_37 - print the char % percentage.
+ * Return: 1.
+ */
+int print_37(void)
+{
+	_putchar(37);
+	return (1);
+}
+
---
+++ print_HEX.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_HEX - prints an hexgecimal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int print_HEX(va_list val)
+{
+	int k;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int tem = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (k = 0; k < counter; k++)
+	{
+		array[k] = tem % 16;
+		tem /= 16;
+	}
+	for (k = counter - 1; k >= 0; k--)
+	{
+		if (array[k] > 9)
+			array[k] = array[k] + 7;
+		_putchar(array[k] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
